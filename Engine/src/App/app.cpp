efa1e96eca136bb8c9f4ae01ba5d7a5d7441b83d
KolKemboi <kolkemboi@gmail.com>
2026-08-25 10:28:23 +0300

    moved the App this is so as to prepare it for static linking later so that the sandbox, to be renamed so, doesnt ship with the engine core

---
+++ Engine/src/App/app.cpp
@@ -0,0 +1,10 @@
+#include "APE_engine.hpp"
+#include <memory>
+
+int main() {
+  // Asimov Physics Engine (A.P.E.)
+  // Make a new app ptr
+  std::unique_ptr<Engine> Asimov(new Engine("A.P.E."));
+  Asimov->Run();
+  Asimov->Clean();
+}
