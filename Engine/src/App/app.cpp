2b51e8bc8a8436412e3f8c5ba8dc0fe166eefd14
KolKemboi <kolkemboi@gmail.com>
2026-09-05 10:57:27 +0300

    Update

--- Engine/src/App/app.cpp
+++ Engine/src/App/app.cpp
@@ -1,10 +1,9 @@
 #include "APE_window.hpp"
-#include <memory>
 
 int main() {
   // Asimov Physics Engine (A.P.E.)
-  // Make a new app ptr
-  std::unique_ptr<APE_Window> Asimov(new APE_Window(960, 720, "A.P.E."));
-  Asimov->RunEngine();
-  Asimov->CleanUp();
+	// Use the stack when possible
+  APE_Window Asimov = APE_Window(960, 720, "A.P.E.");
+  Asimov.RunEngine();
+  Asimov.CleanUp();
 }
--- Engine/src/toolbox/impl/Shader.cpp
+++
@@ -1,9 +0,0 @@
-#include "../header/Shader.hpp"
-
-Shader::Shader(const char *vertexShader, const char *fragmentShader) {}
-
-void Shader::ShaderCleanup() {}
-
-void Shader::UseShaderProgram() const { glUseProgram(m_ShaderProgram); }
-
-unsigned int Shader::GetShaderProgram() const { return m_ShaderProgram; }
